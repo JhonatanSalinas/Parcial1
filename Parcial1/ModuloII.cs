a8a1829830af63d7d8034ad7583978a70b71d3c7
SantiagoA15 <sac2001_@hotmail.com>
2021-09-23 22:20:47 -0500

    Modulo II

---
+++ Parcial1/ModuloII.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial1
+{
+    public partial class ModuloII : Form
+    {
+        public ModuloII()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b;
+            double r;
+            a = Convert.ToInt16(textBox1.Text);
+            b = Convert.ToInt16(textBox2.Text);
+            r = (a * b) / 2;
+            textBox3.Text = Convert.ToString(r);
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
