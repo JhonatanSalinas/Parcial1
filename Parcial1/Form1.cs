44181460bce825df5b04474242bd1b1c386d8e00
JHONATAN CAMILO SALINAS LOPEZ <jcamilosalinas@ucundinamarca.edu.co>
2021-09-23 22:29:10 -0500

    Modulo 1

--- Parcial1/Form1.cs
+++ Parcial1/Form1.cs
@@ -19,33 +19,22 @@ namespace Parcial1
 
         private void button3_Click(object sender, EventArgs e)
         {
-            Numero1.Text = "";
-            Numero2.Text = "";
-            Resultado.Text = "";
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-            int Num1;
-            int Num2;
-            int Res;
-            Num1 = Convert.ToInt32(Numero1.Text);
-            Num2 = Convert.ToInt32(Numero2.Text);
-            if (Num1 > Num2)
-            {
-                Res = Num1;
-                Resultado.Text = Res.ToString();
-            }
-            else
-            {
-                Res = Num2;
-                Resultado.Text = Res.ToString();
-            }
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            this.Close();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            Menu Modulo1 = new Menu();
+            Modulo1.Show();
         }
     }
 }
--- Parcial1/Menu.Designer.cs
+++ Parcial1/Menu.Designer.cs
@@ -29,114 +29,138 @@ namespace Parcial1
         /// </summary>
         private void InitializeComponent()
         {
-            this.button6 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button8 = new System.Windows.Forms.Button();
+            this.Resultado = new System.Windows.Forms.TextBox();
+            this.Numero2 = new System.Windows.Forms.TextBox();
+            this.Numero1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button9 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button6
+            // button7
             // 
-            this.button6.Location = new System.Drawing.Point(392, 156);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(75, 23);
-            this.button6.TabIndex = 17;
-            this.button6.Text = "Modulo 6";
-            this.button6.UseVisualStyleBackColor = true;
+            this.button7.Location = new System.Drawing.Point(100, 362);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 44;
+            this.button7.Text = "Limpiar";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
-            // button5
+            // label5
             // 
-            this.button5.Location = new System.Drawing.Point(392, 127);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(75, 23);
-            this.button5.TabIndex = 16;
-            this.button5.Text = "Modulo 5";
-            this.button5.UseVisualStyleBackColor = true;
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(358, 101);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(65, 20);
+            this.label5.TabIndex = 43;
+            this.label5.Text = "Numero 2";
             // 
-            // label3
+            // label4
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(29, 416);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(150, 13);
-            this.label3.TabIndex = 15;
-            this.label3.Text = "Cenon Santiago Castro Acuña";
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(166, 101);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(65, 20);
+            this.label4.TabIndex = 42;
+            this.label4.Text = "Numero 1";
             // 
-            // label2
+            // button8
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(29, 392);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(154, 13);
-            this.label2.TabIndex = 14;
-            this.label2.Text = "Jhonatan Camilo Salinas Lopez";
+            this.button8.Location = new System.Drawing.Point(288, 190);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 41;
+            this.button8.Text = "Calcular ";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
-            // label1
+            // Resultado
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(315, 37);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(152, 13);
-            this.label1.TabIndex = 13;
-            this.label1.Text = "Selecciona el modulo deseado";
+            this.Resultado.Location = new System.Drawing.Point(272, 234);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(100, 20);
+            this.Resultado.TabIndex = 40;
             // 
-            // button4
+            // Numero2
             // 
-            this.button4.Location = new System.Drawing.Point(392, 98);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(75, 23);
-            this.button4.TabIndex = 12;
-            this.button4.Text = "Modulo 4";
-            this.button4.UseVisualStyleBackColor = true;
+            this.Numero2.Location = new System.Drawing.Point(362, 137);
+            this.Numero2.Name = "Numero2";
+            this.Numero2.Size = new System.Drawing.Size(100, 20);
+            this.Numero2.TabIndex = 39;
             // 
-            // button3
+            // Numero1
             // 
-            this.button3.Location = new System.Drawing.Point(271, 156);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 11;
-            this.button3.Text = "Modulo 3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.Numero1.Location = new System.Drawing.Point(170, 137);
+            this.Numero1.Name = "Numero1";
+            this.Numero1.Size = new System.Drawing.Size(100, 20);
+            this.Numero1.TabIndex = 38;
             // 
-            // button2
+            // label6
             // 
-            this.button2.Location = new System.Drawing.Point(271, 127);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 10;
-            this.button2.Text = "Modulo 2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(358, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(319, 20);
+            this.label6.TabIndex = 37;
+            this.label6.Text = "deberá indicar si el primero o el segundo es el menor.";
             // 
-            // button1
+            // label7
             // 
-            this.button1.Location = new System.Drawing.Point(271, 98);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 9;
-            this.button1.Text = "Modulo 1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Arial Narrow", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(34, 57);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(329, 20);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "El usuario introducirá dos números enteros el programa";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Algerian", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(287, 19);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(122, 26);
+            this.label8.TabIndex = 35;
+            this.label8.Text = "Modulo 1";
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(549, 362);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 34;
+            this.button9.Text = "Atras";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button6);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(709, 445);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.Numero2);
+            this.Controls.Add(this.Numero1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.button9);
             this.Name = "Menu";
-            this.Text = "Menu";
+            this.Text = "Modulo I";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -144,14 +168,16 @@ namespace Parcial1
 
         #endregion
 
-        private System.Windows.Forms.Button button6;
-        private System.Windows.Forms.Button button5;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button4;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.TextBox Resultado;
+        private System.Windows.Forms.TextBox Numero2;
+        private System.Windows.Forms.TextBox Numero1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button9;
     }
 }
